a0227c3dbaea4a2496d0af13318e2b8cbb113baf
asp <asp@aptech.com>
2024-01-16 20:02:25 +0500

    save

---
+++ five.js
@@ -0,0 +1,25 @@
+let heroes = ["superman" , "bat man" , "spiderman" , "hulk" , "iron man"];
+console.log(heroes);
+console.log(heroes[3]);
+
+// ARRAY METHODS
+// LAST INDEX MEI ADD KARTA HAI
+console.log(heroes.push("acha man"))
+console.log(heroes.lenghth);
+console.log(heroes);
+//last index element ko remove karta ha
+console.log(heroes.pop());
+console.log(heroes);
+
+// shift first element ko remove karta hai
+
+console.log(heroes.shift());
+
+// unshift first element me add karta hai
+
+console.log(heroes.unshift("why man"));
+console.log(heroes)
+
+//slice
+
+console.log(heroes.slice(1,4));
---
+++ six.js
@@ -0,0 +1,8 @@
+let fruits = ["apple","mango","dates","orange","pine apple","kiwi","cherry"]
+console.log(fruits);
+
+console.log(fruits.slice(1,2))
+console.log(fruits);
+
+console.log(fruits.splice(3,1, "lychee" , "banana"));
+console.log(fruits);
