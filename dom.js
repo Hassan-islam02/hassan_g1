6e6cd7fbd4a1e06a95ff56c4ef1c7b4bbeec1b2b
asp <asp@aptech.com>
2024-01-23 20:48:07 +0500

    save

--- dom.js
+++ dom.js
@@ -15,3 +15,28 @@ elem.classList.add(yes);
 
 elem.classList.remove(yes);
 
+let mydiv = documnet.getElementsByClassName('container');
+
+mydiv[0].innerHTML = "hello world";
+mydiv[0].style.fontweight = "bold";
+mydiv[0].style.fontsize = "50px";
+
+mydiv[0].classList.add = ('wow');
+
+let newelem = document.createElement('p');
+mydiv[0].appendChild(newelem);
+newelem.innerHTML = "HELLO"
+
+let newbtn = documnet.createElement('button');
+
+newbtn.innerHTML = "submit";
+newbtn.style.padding = "20px 24px"
+newbtn.className = "btn btn-dark"
+newbtn.style.fontsize = "24ox"
+
+// EVENT
+newbtn.Eventlistner('click' , togglebtn);
+function togglebtn(){
+    console.log("you Clicked on me!")
+    newbtn.innerHTML = "wow";
+}
